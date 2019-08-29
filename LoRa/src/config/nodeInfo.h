9cbacbdaa87fd5749a5906615e59a0527ecabe14
Vivek Anchalia <Vivek.Anchalia@microchip.com>
2019-08-28 23:34:27 -0700

    Added instructions for MiWi and LoRaWAN WSN Network, Added LoRaWAN Application Demo

---
+++ LoRa/src/config/nodeInfo.h
@@ -0,0 +1,17 @@
+/*
+ * nodeInfo.h
+ *
+ * Created: 7/12/2019 3:01:33 PM
+ *  Author: C17143
+ */ 
+
+
+#ifndef NODEINFO_H_
+#define NODEINFO_H_
+
+#include "conf_app.h"
+
+
+
+
+#endif /* NODEINFO_H_ */
